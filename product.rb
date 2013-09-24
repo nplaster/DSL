d72bdd87ba7d3626b55c4d330f3eeaf3d1a2719b
Brandon Bosso <brbosso@mines.edu>
2013-09-24 11:24:47 -0600

    Product.rb updated, correctly adds processes, checks if valid process, and handles payment

--- product.rb
+++ product.rb
@@ -15,26 +15,51 @@ class Product
   end
 
 #handle adding a rule with parameters and without
-  def add_rule(text)
-    @rules << Process.new(text)
+  def add_rule(method, noParam, parameter = "")
+    symbol = method.to_sym
+    if respond_to? symbol
+      if !noParam
+      rule = "#{method}"
+      else
+        rule = "#{method} '#{parameter}'"
+      end
+      @rules << rule
+    else
+      abort("Invalid method name #{method}")
+    end
   end
   
 #Handles payment for the product
   def handlePayment()
+    puts "Processing payment of type: #{@text}"
     for rule in @rules
-      rule.handleProcess
+      #puts rule
+      instance_eval(rule)
     end
     
   end
-end
-
-class Processes
-  attr_reader :text
-  def initialize(text)
-    @text = text
+  
+  def packing_slip(text)
+    puts "----Packing slip for #{text}"
   end
   
-  def handleProcess()
-    puts @text
+  def pay(text)
+    puts "----Paying #{text}"
+  end
+  
+  def email(text)
+    puts "----Sending email for #{text}"
+  end
+  
+  def activate()
+    puts "----Activating membership"
+  end
+  
+  def upgrade()
+    puts "----Upgrading membership"
+  end
+  
+  def add_first_aid()
+    puts "----Adding free First Aid video to packing slip"
   end
 end
